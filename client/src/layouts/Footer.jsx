cd02a40fa199fb36231d6a92883ad96ff0036ba0
vnhan28 <vnhan2808@gmail.com>
2025-05-13 12:33:58 +0700

    update

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,44 +1,38 @@
 import { IconButton, Tooltip, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
 import GitHubIcon from '@mui/icons-material/GitHub';
-
 
 const Footer = () => {
   return (
-    <>
+    <footer style={{
+      borderTop: '1px solid #ccc',
+      paddingTop: "32px",
+      paddingBottom: "32px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+      height: "2.5rem",
+      gap: "16px",
+    }}>
+      <Typography variant="h5" sx={{ fontWeight: "600" }}>
+        Copyright © 2024, Nhan Vo
+      </Typography>
 
-      <footer style={{
-        borderTop: '1px solid #ccc',
-        paddingTop: "32px",
-        paddingBottom: "32px",
-        display: "flex", alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
-        bottom: 0,
-        width: " 100%",
-        height: "2.5rem",
-      }} >
-
-        <Typography variant="h5" sx={{
-          fontWeight: "600",
-        }}>
-          Copyright © 2024, Nhan Vo
-        </Typography>
-
-        <Link to="https://github.com/NhanVo288" >
-          <a target="_blank">
-            <Tooltip title='github'>
-              <IconButton color='primary'>
-                <GitHubIcon />
-              </IconButton>
-            </Tooltip>
-          </a>
-        </Link>
-      </footer>
-    </>
+      <a
+        href="https://github.com/NhanVo288"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Tooltip title='GitHub'>
+          <IconButton color='primary'>
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
+      </a>
+    </footer>
   );
 };
 
 export default Footer;
-
-
